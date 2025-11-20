bd95ce91e43ef77a024cb3d5b26e4a1c6b38e072
Iggu18 <ignatiousbenoyputhettu100@gmail.com>
2025-11-20 16:06:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+function draw() {
+  background('#F5EAB3'); 
+
+
+  noStroke();
+  fill('#3A5A40'); 
+  triangle(50, 350, 200, 100, 350, 350); 
+
+  fill('#B85C38'); 
+  triangle(0, 400, 200, 250, 400, 400); 
+}
